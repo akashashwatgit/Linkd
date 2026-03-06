961393f2bc580b53b21285b26881691c88e02173
akashashwatgit <173206486+akashashwatgit@users.noreply.github.com>
2026-03-07 00:37:03 +0530

    deployment ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from 'path'
 
 dotenv.config();
 const app=express();
@@ -11,7 +12,17 @@ const PORT=process.env.PORT;
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-
+const __dirname=path.resolve()
 
 
 app.listen(PORT,()=> console.log(`Server is running on port ${PORT}`));
+
+//Deploying on sevalla
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')))
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,'../frontend/dist/index.html'))
+    })
+}
