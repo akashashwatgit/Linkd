9b35028a06e1cfd44a7813b47a29f3b0a84ca60d
akashashwatgit <shashwat310305@gmail.com>
2026-06-15 14:16:39 +0530

    Refactor checkAuth method for clarity

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -14,17 +14,20 @@ export const useAuthStore = create((set, get) => ({
   onlineUsers: [],
 
   checkAuth: async () => {
-    try {
-      const res = await axiosInstance.get("/auth/check");
-      set({ authUser: res.data });
-      get().connectSocket();
-    } catch (error) {
-      console.log("Error in authCheck:", error);
-      set({ authUser: null });
-    } finally {
-      set({ isCheckingAuth: false });
-    }
-  },
+  try {
+    const res = await axiosInstance.get("/auth/check");
+
+    set({ authUser: res.data });
+
+    // Connect socket after auth succeeds
+    get().connectSocket();
+  } catch (error) {
+    console.log("Error in authCheck:", error);
+    set({ authUser: null });
+  } finally {
+    set({ isCheckingAuth: false });
+  }
+},
 
   signup: async (data) => {
     set({ isSigningUp: true });
